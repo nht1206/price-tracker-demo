fdb7915a56f684524d9f8d97fbdf5bb4e978bbec
nht1206 <nghuutho74@gmail.com>
2021-11-27 13:43:50 +0700

    Update unit test

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -1,7 +1,6 @@
 package logger
 
 import (
-	"os"
 	"testing"
 
 	"github.com/nht1206/pricetracker/config"
@@ -9,17 +8,10 @@ import (
 
 func TestInitLogger(t *testing.T) {
 	err := InitLogger(&config.LogConfig{
-		OutputPath: "",
-		FileName:   "test.log",
-		Level:      "info",
+		Level: "info",
 	})
 
 	if err != nil {
 		t.Errorf("failed to initialize logger. %v", err)
-	}
-
-	err = os.Remove("test.log")
-	if err != nil {
-		t.Errorf("failed to delete log file. %v", err)
 	}
 }
